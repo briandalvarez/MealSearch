0308fc4baf5b342767979672953ced8838091284
briandalvarez <briandalvarez520@gmail.com>
2025-10-19 15:42:52 -0700

    Initial Commit

---
+++ MealSearch/MealSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealSearchApp.swift
+//  MealSearch
+//
+//  Created by Brian Alvarez on 10/19/25.
+//
+
+import SwiftUI
+
+@main
+struct MealSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
