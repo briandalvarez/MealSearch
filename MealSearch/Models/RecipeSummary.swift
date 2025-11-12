da65b0c1f21f38a4bfe9811d07b3c3989779f458
briandalvarez <briandalvarez520@gmail.com>
2025-11-12 14:40:38 -0800

    Added the app icon. Created a temporary API test to retrieve all the recipe card information for ten pasta recipes just to see if the spoonacular API can actually provide the information we need. The test was succesful. This file can be deleted before the next push. Also created a RecipeSummary file for decoding API data and turning it into useable Swift objects.

---
+++ MealSearch/Models/RecipeSummary.swift
@@ -0,0 +1,8 @@
+//
+//  RecipeSummary.swift
+//  MealSearch
+//
+//  Created by Brian Alvarez on 11/12/25.
+//
+
+import Foundation
---
+++ MealSearch/Networking/APITest.swift
@@ -0,0 +1,8 @@
+//
+//  APITest.swift
+//  MealSearch
+//
+//  Created by Brian Alvarez on 11/11/25.
+//
+
+import Foundation
